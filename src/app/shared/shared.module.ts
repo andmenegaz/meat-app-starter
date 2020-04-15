a3a92ee4c0a19396f4cefa5e3f93db79b68453c1
andmenegaz <andre.menegaz@litma.com.br>
2020-04-15 11:28:31 -0300

    Items Animations

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { InputComponent } from './input/input.component';
+import { RadioComponent } from './radio/radio.component';
+import { RatingComponent } from './rating/rating.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { OrderService } from 'app/order/order.service';
+import { SnackbarComponent } from 'app/shared/messages/snackbar/snackbar.component';
+import { NotificationService } from './messages/notification.service';
+
+@NgModule({
+    declarations: [ InputComponent, RadioComponent, RatingComponent, SnackbarComponent ],
+    imports: [ ReactiveFormsModule, CommonModule, FormsModule ],
+    exports: [
+        InputComponent, 
+        RadioComponent, 
+        RatingComponent, 
+        CommonModule, 
+        FormsModule, 
+        ReactiveFormsModule,
+        SnackbarComponent],
+    providers: [],
+})
+export class SharedModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [ShoppingCartService, RestaurantsService, OrderService, NotificationService]
+        }
+    }
+}
